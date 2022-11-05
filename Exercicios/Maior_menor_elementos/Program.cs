1675ba4370eed986839efbdd478c5f19d67a02ff
FlavioMartinsCruz <flaviomartinscruz@gmail.com>
2022-11-05 17:55:18 -0300

    Resolução de exercícios propostos

--- Exercicios/Maior_menor_elementos/Program.cs
+++ Exercicios/Maior_menor_elementos/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Ler um vetor com 10 inteiros e descubra o maior e o menor elemento do vetor.
 
+Console.Clear();
+int[] numeros = new int[10];
+String[] listaAuxiliar = new String[10];
 
+System.Console.WriteLine("Digite 10 números inteiros:");
+
+for(int i=0; i<10; i++)
+{
+  System.Console.Write($"Número {i+1}: ");
+  numeros[i] = int.Parse(Console.ReadLine()!);
+}
+Array.Sort(numeros);
+foreach(int i in numeros)
+{
+  System.Console.WriteLine(i);
+}
+Console.Clear();
+System.Console.WriteLine($"O menor número é o {numeros[0]}.");
+System.Console.WriteLine($"O maior número é o {numeros[9]}.");
