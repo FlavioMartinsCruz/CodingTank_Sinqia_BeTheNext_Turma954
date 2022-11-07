f2e09e91c4a9df808b246ee42444bb26b2c8ae74
FlavioMartinsCruz <flaviomartinscruz@gmail.com>
2022-11-06 22:08:47 -0300

    Adição de exercício proposto

---
+++ Exercicios/Cadastro_biblioteca/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Crie um cadastro de uma biblioteca, que leia os seguintes dados:
+    Título, Subtitulo, Autor, Editora, Número de páginas, Data de publicação, Idioma.
+    Após receber os dados, limpe o console e exiba os dados na tela.
+    Altere o cadastro da biblioteca para exibir os dados na seguinte formatação:
+    Livro cadastrado com sucesso!
+    Livro: Titulo - Subtitulo
+    Autor: Nome do autor
+    Editora: Editora
+    Detalhes: Publicado em 15/07/2001 com 99 páginas, em Português.
+*/
